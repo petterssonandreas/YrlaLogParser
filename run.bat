72518ac65807b526b077c2ee14340bb7dff2e5d9
Andreas Pettersson <andreaspettersson95@gmail.com>
2018-08-16 16:38:57 +0200

    run.bat now installs needed packages.

--- run.bat
+++ run.bat
@@ -1,6 +1,10 @@
 @ECHO OFF
 ECHO Running YrlaLogParser python program.
 ECHO Will open a browser tab with results when done.
+
+ECHO Installing packages needed for Google authentication and access.
+pip install --upgrade google-api-python-client
+pip install --upgrade oauth2client
 ECHO Working...
 python main.py
 ECHO Done!
